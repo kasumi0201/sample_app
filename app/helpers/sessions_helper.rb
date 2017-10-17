e7ede3a1672be7bf0615a1771226974d45507146
kasumi <kasumi0201@gmail.com>
2017-10-17 22:40:14 +1100

    9

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,12 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  # ユーザーのセッションを永続的にする
+def remember(user)
+  user.remember
+  cookies.permanent.signed[:user_id] = user.id
+  cookies.permanent[:remember_token] = user.remember_token
+end
   # 現在ログイン中のユーザーを返す (いる場合)
  def current_user
   #  Rubyでは、「変数の値がnilなら変数に代入するが、nilでなければ代入しない (変数の値を変えない)」という操作が非常によく使われます。(https://railstutorial.jp/chapters/basic_login?version=5.1#cha-basic_login)
@@ -12,17 +18,33 @@ module SessionsHelper
   # => "bar"
   # >> @foo = @foo || "baz"
   # => "bar"
-   @current_user ||= User.find_by(id: session[:user_id])
-
+  if (user_id = session[:user_id])
+    @current_user ||= User.find_by(id: user_id)
+  elsif (user_id = cookies.signed[:user_id])
+        # テストがパスすれば、この部分がテストされていないことがわかる
+    user = User.find_by(id: user_id)
+    if user && user.authenticated?(cookies[:remember_token])
+      log_in user
+      @current_user = user
+    end
+end
  end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
-  # 現在のユーザーをログアウトする
-def log_out
-  session.delete(:user_id)
-  @current_user = nil
-end
+  # 永続的セッションを破棄する
+   def forget(user)
+     user.forget
+     cookies.delete(:user_id)
+     cookies.delete(:remember_token)
+   end
+
+   # 現在のユーザーをログアウトする
+   def log_out
+     forget(current_user)
+     session.delete(:user_id)
+     @current_user = nil
+   end
 end
